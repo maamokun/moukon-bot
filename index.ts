947da20651ef2710fc28aeeb15f99e48d0e5267b
maamokun/MikanDev <maamo-kun@outlook.com>
2024-06-06 00:37:44 +0900

    time fix

--- index.ts
+++ index.ts
@@ -4,7 +4,7 @@ import { postImage } from "./scripts/post";
 
 // Schedule task to run at 12AM JST every day
 const rule = new schedule.RecurrenceRule();
-rule.hour = moment().tz("Asia/Tokyo").hour(0).minute(0).second(0).toDate().getHours();
+rule.hour = moment().tz("Asia/Tokyo").hour(9).minute(0).second(0).toDate().getHours();
 rule.minute = 0;
 rule.tz = 'Asia/Tokyo';
 
